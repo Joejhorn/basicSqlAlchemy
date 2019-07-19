28295c068c049d3c0d2cb98e4ad92b4b6fa53e9a
Joe Horn <joejhorn@yahoo.com>
2019-07-18 19:35:13 -0700

    added migrations

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-#__file__ is set to the file you are working on
+# __file__ is set to the file you are working on
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -14,20 +15,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+Migrate(app, db)
+
 
 class Puppy(db.Model):
-  __tablename__ = 'puppies'
+    __tablename__ = 'puppies'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.Text)
-  age = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
 
-  def __repr__(self):
-    return f"Puppy {self.name} is {self.age}"
+    def __repr__(self):
+        return f"Puppy {self.name} is {self.age}"
 
 
 # if __name__ == '__main__':
